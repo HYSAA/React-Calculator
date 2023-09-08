7579161bc1e8b7c93bac2d7d2a2002c4916af399
HYSA <aj.anunciado@gmail.com>
2023-09-08 15:39:26 +0800

    asd

--- react calculator/src/App.jsx
+++ react calculator/src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React,{ useState } from 'react'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// const Calculator = () =>{
+
+//   const[firstNumber,setFirstNumber]=useState("")
+//   const[secondNumber,setSecondNumber]=useState("")
+//   let [sum,setSum]=useState("")
+  
+//   const Setadd= () =>{
+//   const a= parseInt(firstNumber);
+//   const b= parseInt(secondNumber);
+//   setSum(a+b);
+//   console.log(setSum)
+//   }
+//   const Setminus = ()=>{
+
+
+//   }
+//   const Settimes = () =>{
+
+//   }
+//   const Setdivide = () =>{
+
+//   }
+
+
+//   const App = () => {
+//   return(
+//    <div className='Calculator'>
+//     <h1>Enter First Number</h1>
+
+
+
+
+
+//     <button onClick={Calculator}>Calculate</button>
+  
+
+
+
+
+
+  
+//    </div>
+
+  
+
+  
+//   )}
+//   }
+// export default App
+import React, { useState } from "react";
+
+const App = () => {
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+  const [sum, setSum] = useState("");
+
+  const handleAddition = () => {
+    const a = parseInt(firstNumber);
+    const b = parseInt(secondNumber);
+    setSum(a + b);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="Addition">
+      <h1>Enter First Number</h1>
+      <input
+        type="number"
+        placeholder="First Number"
+        onChange={(event) => setFirstNumber(event.target.value)}
+      />
+      <br />
 
-export default App
+      <h1>Enter Second Number</h1>
+      <input
+        type="number"
+        placeholder="Second Number"
+        onChange={(event) => setSecondNumber(event.target.value)}
+      />
+      <br />
+
+      <button onClick={handleAddition}>Add</button>
+      <h2>Result is: {sum}</h2>
+    </div>
+  );
+};
+
+export default App;
