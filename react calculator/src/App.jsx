f5b492c48da6f9fe6a9f47308fbf4e35b0ef131d
HYSA <aj.anunciado@gmail.com>
2023-09-08 20:46:18 +0800

    FINISHED

--- react calculator/src/App.jsx
+++ react calculator/src/App.jsx
@@ -1,89 +1,94 @@
-// import React,{ useState } from 'react'
-// import './App.css'
-
-
-// const Calculator = () =>{
-
-//   const[firstNumber,setFirstNumber]=useState("")
-//   const[secondNumber,setSecondNumber]=useState("")
-//   let [sum,setSum]=useState("")
-  
-//   const Setadd= () =>{
-//   const a= parseInt(firstNumber);
-//   const b= parseInt(secondNumber);
-//   setSum(a+b);
-//   console.log(setSum)
-//   }
-//   const Setminus = ()=>{
-
-
-//   }
-//   const Settimes = () =>{
-
-//   }
-//   const Setdivide = () =>{
-
-//   }
-
-
-//   const App = () => {
-//   return(
-//    <div className='Calculator'>
-//     <h1>Enter First Number</h1>
-
-
-
-
-
-//     <button onClick={Calculator}>Calculate</button>
-  
-
-
-
-
-
-  
-//    </div>
-
-  
-
-  
-//   )}
-//   }
-// export default App
-import React, { useState } from "react";
+import React, {useState} from "react";
+import './App.css';
 
 const App = () => {
-  const [firstNumber, setFirstNumber] = useState("");
-  const [secondNumber, setSecondNumber] = useState("");
+  const [firstNumber, setFirstNumber] = useState(""); 
+  const [secondNumber, setSecondNumber] = useState(""); 
   const [sum, setSum] = useState("");
+  const [sub, setSub]= useState("");
+  const [mul, setMul]=useState("");
+  const [divide,setDivide]=useState("")
 
   const handleAddition = () => {
     const a = parseInt(firstNumber);
     const b = parseInt(secondNumber);
     setSum(a + b);
+    setSub("")
+    setMul("")
+    setDivide("")
+   
   };
+  const handleSubtraction=()=>{
+    const a = parseInt(firstNumber);
+    const b = parseInt(secondNumber);
+    setSum("");
+    setSub(a-b)
+    setMul("")
+    setDivide("")
+   
+  }
+  const handleMultiplication=()=>{
+    const a = parseInt(firstNumber);
+    const b = parseInt(secondNumber);
+    setSum("")
+    setSub("")
+    setMul(a*b)
+    setDivide("")
+   
+  }
+  const handleDivition=()=>{
+    const a = parseInt(firstNumber);
+    const b = parseInt(secondNumber);
+    setSum("")
+    setSub("")
+    setMul("")
+    setDivide(a/b)
+  }
+
 
   return (
     <div className="Addition">
+      <div className="first">
       <h1>Enter First Number</h1>
+      
       <input
         type="number"
         placeholder="First Number"
+        value={firstNumber}
         onChange={(event) => setFirstNumber(event.target.value)}
       />
+      </div>
       <br />
 
       <h1>Enter Second Number</h1>
       <input
         type="number"
         placeholder="Second Number"
+        value={secondNumber} 
         onChange={(event) => setSecondNumber(event.target.value)}
       />
       <br />
 
       <button onClick={handleAddition}>Add</button>
-      <h2>Result is: {sum}</h2>
+      <button onClick={handleSubtraction}>Subtract</button>
+      <button onClick={handleMultiplication}>Multiply</button>
+      <button onClick={handleDivition}>Divide</button>
+      <h2>
+  {sum && `Result: ${sum}`}
+  {sub && `Result: ${sub}`}
+  {mul && `Result: ${mul}`}
+  {divide && `Result: ${divide}`}
+</h2>
+
+<add  handleAddition onset button onClicks
+    add useCallback(
+      () => {
+         firstNumber 
+      },
+      [second],
+      secondNumber
+    )
+    
     </div>
   );
 };
